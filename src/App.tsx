b19a7018c113690189c9b91105c67e42e046c4d3
Ashim <panghalashim7832@gmail.com>
2025-07-11 23:44:12 +0530

    D

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,27 @@ import Layout from './components/layout';
 import { ThemeProvider } from './components/context/theme-provider';
 import WeatherDashBoard from './Pages/WeatherDashBoard';
 import Citypage from './Pages/city-page';
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+const queryClient = new QueryClient();
+
 const App = () => {
   return (
-    <BrowserRouter>
-      <ThemeProvider>
-        <Layout>
-          <Routes>
-            <Route path="/" element={<WeatherDashBoard />} />
-            <Route path="/city/:cityName" element={<Citypage />} />
-          </Routes>
-        </Layout>
-      </ThemeProvider>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      {' '}
+      <BrowserRouter>
+        <ThemeProvider>
+          <Layout>
+            <Routes>
+              <Route path="/" element={<WeatherDashBoard />} />
+              <Route path="/city/:cityName" element={<Citypage />} />
+            </Routes>
+          </Layout>
+        </ThemeProvider>
+      </BrowserRouter>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 };
 export default App;
